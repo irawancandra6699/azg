6c94450e4e0acb062890931129c6b3aea1b55c65
irawancandra6699 <134005006+irawancandra6699@users.noreply.github.com>
2024-02-02 11:36:06 +0700

    Update dashboard.sh

--- dashboard.sh
+++ dashboard.sh
@@ -1,5 +1,62 @@
 #!/bin/bash
 # =========================================
+BURIQ () {
+    curl -sS https://raw.githubusercontent.com/irawancandra6699/izin/main/ip > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f /root/tmp
+}
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS https://raw.githubusercontent.com/irawancandra6699/izin/main/ip | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/irawancandra6699/izin/main/ip | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+red='\e[1;31m'
+green='\e[1;32m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+
+if [ "$res" = "Expired" ]; then
+Exp="\e[36mExpired\033[0m"
+else
+Exp=$(curl -sS https://raw.githubusercontent.com/irawancandra6699/izin/main/ip | grep $MYIP | awk '{print $3}')
+fi
+
 vlx=$(grep -c -E "^#& " "/etc/xray/config.json")
 let vla=$vlx/2
 vmc=$(grep -c -E "^### " "/etc/xray/config.json")
@@ -49,19 +106,6 @@ export Server_IP="underfined"
 export Script_Mode="Stable"
 export Auther="CANDRAvpnz"
 # Getting
-echo "sedang memverifkasi"
-MYIP=$(curl -s ipv4.icanhazip.com);
-CEKEXPIRED () {
-    today=$(date -d +1day +%Y-%m-%d)
-    Exp1=$(curl -sS https://raw.githubusercontent.com/irawancandra6699/izin/main/ip | grep $MYIP | awk '{print $3}')
-    if [[ $today < $Exp1 ]]; then
-echo -e "verifikasi IP di terima"
-    else
-echo -e "\e[31manda di tolak!\e[0m"
-    exit 
-fi
-}
-
 if [ ! -e /tmp/trojan ]; then
   mkdir -p /tmp/trojan
 fi
@@ -74,34 +118,10 @@ if [ ! -e /tmp/vless ]; then
   mkdir -p /tmp/vless
 fi
 
-IZIN=$(curl -sS https://raw.githubusercontent.com/irawancandra6699/izin/main/ip | awk '{print $4}' | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
-echo "status akun masih aktif" 
-CEKEXPIRED 
-else
-echo -e "\e[31mSCRIPT ANDA EXPIRED!\e[0m"
-exit 0
-fi
 # status
 rm -rf /root/status
 wget -q -O /root/status "kvm-xcode.biz.id/azg/statushariini" 
 
-today=`date -d "0 days" +"%Y-%m-%d"`
-Exp2=$(curl -sS https://raw.githubusercontent.com/irawancandra6699/izin/main/ip | grep $MYIP | awk '{print $3}')
-if [ "$Exp2" == "lifetime" ]; then
-    Exp2="2099-12-09"
-fi
-# CERTIFICATE STATUS
-d1=$(date -d "$Exp2" +%s)
-d2=$(date -d "$today" +%s)
-left=$(((d1 - d2) / 86400))
-
-
-datediff() {
-    d1=$(date -d "$1" +%s)
-    d2=$(date -d "$2" +%s)
-    echo -e "$COLOR1 $NC Expiry In   : $(( (d1 - d2) / 86400 )) Days"
-}
 
 systemctl restart fail2ban
 
@@ -120,9 +140,6 @@ wget -q -O updatsc.sh "https://kvm-xcode.biz.id/azg/updateyes.sh" && chmod +x up
 
 # // Exporting IP Address
 export MYIP=$( curl -s ipv4.icanhazip.com )
-Name=$(curl -sS https://raw.githubusercontent.com/irawancandra6699/izin/main/ip | grep $MYIP | awk '{print $2}')
-Exp=$(curl -sS https://raw.githubusercontent.com/irawancandra6699/izin/main/ip | grep $MYIP | awk '{print $3}')
-
 # // nginx
 nginx=$( systemctl status nginx | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
 if [[ $nginx == "running" ]]; then
@@ -236,16 +253,23 @@ echo -e "${GREEN}━━━━━━━━━━━━━━━━━━━━━
 
 echo -e "${GREEN}|\E[44;1;39m                   °Ketik menu 👉°                \E[0m|"
 echo -e "${GREEN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-
-echo -e " ${GREEN}"
-echo -e "${GREEN}┌──────────────────────────────────────────────────┐${NC}"
-echo -e "${GREEN}│ ${BOLD}${LIGHT}Client    = $Name                           ${NC}"
-echo -e "${GREEN}│ ${BOLD}${LIGHT}Expired   = $Exp                           ${NC}"
-echo -e "${GREEN}│ ${BOLD}${LIGHT}remaining = ${YELLOW}$left Days                          ${NC}"
-echo -e "${GREEN}│ ${BOLD}${LIGHT}Developer = KINGSTORE BY CANDRA💯                         ${NC}"
-echo -e "${GREEN}│ ${BOLD}${LIGHT}Version   = 4.0.0 LTS                         ${NC}"
-
-echo -e "${GREEN}└──────────────────────────────────────────────────┘${NC}"
+DATE=$(date +'%d %B %Y')
+datediff() {
+    d1=$(date -d "$1" +%s)
+    d2=$(date -d "$2" +%s)
+    echo -e "        ${BIPurple}│$NC Expiry In     : $(( (d1 - d2) / 86400 )) Days $NC"
+}
+mai="datediff "$Exp" "$DATE""
+echo -e "        ${GREEN}┌─────────────────────────────────────┐${NC}"
+echo -e "        ${GREEN}│$NC Version       : $(cat /opt/.ver) Last Update ${NC}"
+echo -e "        ${GREEN}│$NC ${GREEN}User          :\033[1;36m $Name \e[0m"
+if [ $exp \< 1000 ];
+then
+echo -e "          $GREEN│$NC License      : ${GREEN}$sisa_hari$NC Days Tersisa $NC"
+else
+    datediff "$Exp" "$DATE"
+fi;
+echo -e "        ${GREEN}└─────────────────────────────────────┘${NC}"
 
 
 
